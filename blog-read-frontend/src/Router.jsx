5166239b465c6465ac5c7c1513624185fad11577
Bryan Chow <bchow.post@gmail.com>
2024-05-23 19:41:37 +0100

    Add React Router set up

---
+++ blog-read-frontend/src/Router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+
+const Router = () => {
+	const router = createBrowserRouter([{ path: "/", element: <App /> }]);
+	return <RouterProvider router={router} />;
+};
+
+export default Router;
